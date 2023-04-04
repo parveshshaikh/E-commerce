1da82f7f887bf012a091e434eb463c220a0724aa
Parvesh Shaikh <Parvesh1998shaikh@gmail.com>
2023-04-04 15:17:26 +0530

    s

---
+++ src/Utils/Data.js
@@ -0,0 +1,396 @@
+import I1 from '../img/i1.png'
+import f1 from '../img/f1.png'
+import C3 from '../img/c3.png'
+import Fi1 from '../img/fi1.png'
+import c1 from  '../img/c1.png'
+import c2 from '../img/c2.png'
+import c3 from '../img/c3.png'
+import c4 from '../img/c4.png'
+import c6 from '../img/c6.png'
+import c7 from '../img/c7.png'
+import d7 from '../img/d7.png'
+import d8 from '../img/d8.png'
+import cu1 from '../img/cu1.png'
+import cu2 from '../img/cu2.png'
+import cu3 from '../img/cu3.png'
+import cu4 from '../img/cu4.png'
+import cu5 from '../img/cu5.png'
+import cu6 from '../img/cu6.png'
+import d1 from '../img/d1.png'
+import d2 from '../img/d2.png'
+import d3 from '../img/d3.png'
+import d4 from '../img/d4.png'
+import d5 from '../img/d5.png'
+import d6 from '../img/d6.png'
+import f2 from '../img/f2.png'
+import f3 from '../img/f3.png'
+import f4 from '../img/f4.png'
+import f5 from '../img/f5.png'
+import f6 from '../img/f6.png'
+import f7 from '../img/f7.png'
+import f8 from '../img/f8.png'
+import f9 from '../img/f9.png'
+import f10 from '../img/f10.png'
+import fi1 from '../img/fi1.png'
+import fi2 from '../img/fi2.png'
+import fi3 from '../img/fi3.png'
+import fi4 from '../img/fi4.png'
+import fi5 from '../img/fi5.png'
+import i1 from '../img/i1.png'
+import i2 from '../img/i2.png'
+import i3 from '../img/i3.png'
+import i4 from '../img/i4.png'
+import i5 from '../img/i5.png'
+import i6 from '../img/i6.png'
+import i7 from '../img/i7.png'
+import r1 from '../img/r1.png'
+import r2 from '../img/r2.png'
+import r3 from '../img/r3.png'
+import r4 from '../img/r4.png'
+import r5 from '../img/r5.png'
+
+export const herosData=[
+          {
+                    id:1,name:'icecream',desc:'Chocolate',price :'5.25',
+                    image:I1
+          },
+          {
+                    id:2,name:'icecream',desc:'Straberry',price :'5.25',
+                    image:f1
+          },
+          {
+                    id:3,name:'icecream',desc:'Mix Kebab plate',price :'5.25',
+                    image:C3
+          },
+          {
+                    id:4,name:'icecream',desc:'Fish Kebab',price :'5.25',
+                    image:Fi1
+          },
+]
+
+
+export const cat=[
+          {
+                    id:11,name:'icecream',desc:'Chocolate',
+                    price :'5.25',
+                    image:I1
+          },
+          {
+                    id:12,name:'icecream',desc:'Straberry',
+                    price :'5.25',
+                    image:f1
+          },
+          {
+                    id:13,name:'icecream',desc:'Mix Kebab plate',price :'5.25',
+                    image:C3
+          },
+          {
+                    id:14,name:'icecream',desc:'Fish Kebab',
+                    price :'5.25',
+                    image:Fi1
+          },
+]
+export const product=[
+    {
+        id:1,name:' manchurian',category:"chicken",
+        price :'6 ',
+        image:c1,
+        qty:0  
+    },
+    {
+        id:2,name:'chicken 65',category:"chicken",
+        price :'7 ',
+        image:c2,
+        qty:0
+    },
+    {
+        id:3,name:'tandoori platter',category:"chicken",
+        price :'5 ',
+        image:c3,
+        qty:0
+    },
+    {
+        id:4,name:'fried chicken',category:"chicken",
+        price :'6.25',
+        image:c4,
+        qty:0
+    },
+    {
+        id:5,name:'chicken wings ',category:"chicken",
+        price :'8 ',
+        image:c6,
+        qty:0
+    },
+    {
+         id:6,name:'chicken & fries',category:"chicken",
+        price :'8 ',
+        image:c7,
+        qty:0
+    },
+    {
+         id:7,name:'mango mousse pudding',category:"drink",
+        price :'8 ',
+        image:d7,
+        qty:0
+    },
+    {
+         id:8,name:'coca cola',category:"drink",
+        price :'8 ',
+        image:d8,
+        qty:0
+    },
+    {
+          id:9,name:'butter chicken',category:"chicken",
+        price :'8 ',
+        image:cu1,
+        qty:0
+    },
+    {
+          id:10,name:'mix veg curry',category:"veg curry",
+        price :'8 ',
+        image:cu2,
+        qty:0
+    },
+    {
+          id:11,name:'shrimp curry',category:"fish",
+        price :'8 ',
+        image:cu3,
+        qty:0
+    },
+    {
+            id:12,name:'chicken curry',category:"chicken",
+        price :'8 ',
+        image:cu4,
+        qty:0
+    },
+    {
+            id:13,name:'mutton curry',category:"chicken",
+        price :'8 ',
+        image:cu5,
+        qty:0
+    },
+    {
+            id:14,name:'tom yum soup & rich',category:"soup & rich",
+        price :'8 ',
+        image:cu6,
+        qty:0
+    },
+    {
+            id:15,name:'red bull',category:"drink",
+        price :'8 ',
+        image:d1,
+        qty:0
+    },
+    {
+            id:16,name:'pepsi',category:"drink",
+        price :'8 ',
+        image:d2,
+        qty:0
+    },
+    {
+            id:17,name:'monster energy',category:"drink",
+        price :'8 ',
+        image:d3,
+        qty:0
+    },
+    {
+            id:18,name:'blue hawaii',category:"drink",
+        price :'8 ',
+        image:d4,
+        qty:0
+    },
+    {
+            id:19,name:'sprite',category:"drink",
+        price :'8 ',
+        image:d5,
+        qty:0
+    },
+    {
+            id:20,name:'fanta orange',category:"drink",
+        price :'8 ',
+        image:d6,
+        qty:0
+    },
+    {
+             id:21,name:'strawberry',category:"fruit",
+        price :'8 ',
+        image:f1,
+        qty:0
+    },
+    {
+             id:22,name:'pineapple',category:"fruit",
+        price :'8 ',
+        image:f2,
+        qty:0
+    },
+    {
+              id:23,name:'black berry',category:"fruit",
+        price :'8 ',
+        image:f3,
+        qty:0
+    },
+    {
+              id:24,name:'grapes',category:"fruit",
+        price :'8 ',
+        image:f4,
+        qty:0
+    },{
+           id:25,name:'strawberry',category:"fruit",
+        price :'8 ',
+        image:f5,
+        qty:0
+    },
+    {
+          id:26,name:'cranberry',category:"fruit",
+        price :'8 ',
+        image:f6,
+        qty:0
+    },
+    {
+          id:27,name:'raspberry',category:"fruit",
+        price :'8 ',
+        image:f7,
+        qty:0
+    },
+    {
+          id:28,name:'cherry',category:"fruit",
+        price :'8 ',
+        image:f8,
+        qty:0
+    },
+    {
+          id:29,name:'banana',category:"fruit",
+        price :'8 ',
+        image:f9,
+        qty:0
+    },
+    {
+          id:30,name:'watermelon',category:"fruit",
+        price :'8 ',
+        image:f10,
+        qty:0
+    },
+    {
+          id:31,name:'sakura susi',category:"fish",
+        price :'8 ',
+        image:fi1,
+        qty:0
+    },
+    {
+         id:32,name:'grilled fish',category:"fish",
+        price :'8 ',
+        image:fi2,
+        qty:0
+    },
+    {
+           id:33,name:'grilled fish',category:"fish",
+        price :'8 ',
+        image:fi3,
+        qty:0
+    },
+    {
+           id:34,name:'grilled fish',category:"fish",
+        price :'8 ',
+        image:fi4,
+        qty:0
+    },
+    {
+           id:35,name:'toothfish',category:"fish",
+        price :'8 ',
+        image:fi5,
+        qty:0
+    },
+    {
+           id:36,name:'sundae chocolat',category:"ice cream",
+        price :'8 ',
+        image:i1,
+        qty:0
+    },
+    {
+           id:37,name:'sundae',category:"ice cream",
+        price :'8 ',
+        image:i2,
+        qty:0
+    },
+    {
+           id:38,name:'ice cream cone',category:"ice cream",
+        price :'8 ',
+        image:i3,
+        qty:0
+    },
+    {
+           id:39,name:'filling frozen desserts',category:"ice cream",
+        price :'8 ',
+        image:i4,
+        qty:0
+    },
+    {
+           id:40,name:'strawberry ice cream',category:"ice cream",
+        price :'8 ',
+        image:i5,
+        qty:0
+    },
+    {
+           id:41,name:'ice cream cup',category:"ice cream",
+        price :'8 ',
+        image:i6
+    },
+    {
+           id:42,name:'strawberry ice cream ',category:"ice cream",
+        price :'8 ',
+        image:i7,
+        qty:0
+    },
+    {
+           id:43,name:'veg dum biryani',category:"rice",
+        price :'8 ',
+        image:r1,
+        qty:0
+    },
+  
+    
+]
+
+
+
+      export const category=[
+        {
+            id:1,name:"chicken"
+        },   
+        {
+             id:10,name:"veg curry",
+            
+        },
+     
+        {
+             id:14,name:"soup & rich",
+            
+        },
+        {
+             id:15,name:"drink",
+            
+        },
+      
+        {
+             id:21,name:"fruit",
+            
+        },
+       
+        {
+             id:31,name:"fish",
+            
+        },
+        
+        {
+             id:36,name:"ice cream",
+            
+        },
+        
+        {
+             id:43,name:"rice",
+            
+        },
+      
+      
+        
+    ]
